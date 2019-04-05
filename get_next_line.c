7f86b123b2981c59645d5b2312b35ae6db257c48
Rmmshv <32555704+Rmmshv@users.noreply.github.com>
2019-04-04 17:13:03 -0700

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,14 @@
+#include "get_next_line.h"
+
+static char		*read_line(const int fd, char *buffer, int *rtrn)
+{
+	char	tmp[BUFF_SIZE + 1];
+	char	*tmp2;
+
+
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	
+}
---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include "libft/libft.h"
+#include <unistd.h>
+
+# define BUFF_SIZE	1
+
+# define ENDOFL		'\n'
+
+int		get_next_line(const int fd, char **line)
+
+#endif
