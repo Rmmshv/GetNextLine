915fd1958991dc5f43370d724e0310efa6ffc41a
Rmmshv <32555704+Rmmshv@users.noreply.github.com>
2019-04-07 17:49:10 -0700

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -4,11 +4,11 @@ static char		*read_line(const int fd, char **buffer)
 {
 	char	tmp[BUFF_SIZE + 1];
 	char	*tmp2;
-	int		read_res;
+	int		r_rslt;
 
 	while ((read_res = read(fd, tmp, BUFF_SIZE)))
 	{
-		tmp[read_res] = '\0';
+		tmp[r_reslt] = '\0';
 		tmp2 = *buffer;
 		if (!(buffer = ft_strjoin(*buffer, tmp)))
 			return (-1);
@@ -16,9 +16,7 @@ static char		*read_line(const int fd, char **buffer)
 		if (ft_strchr(tmp, ENDOFL))
 			break;
 	}
-	return (read_res);
-
-
+	return (r_rslt);
 }
 
 int		ft_endcpy(char **buffer, char **line)
